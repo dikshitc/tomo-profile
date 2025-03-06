25814e5884cd0e292e17b45e82a48a869b7baef8
dikshitc <dikshitcnagiri@gmail.com>
2025-03-06 15:03:12 +0530

    Updated feature

--- src/components/AboutHeroSection.jsx
+++ src/components/AboutHeroSection.jsx
@@ -14,7 +14,7 @@ const AboutHeroSection = () => {
           She has worked on ODA funding NGO projects for the Consulate General of Japan, coordinated Japan's first official emergency relief post-Gujarat Earthquake of 2001, and served as the only Japanese expert for the People's Linguistic Survey of India.
         </p>
         <p>
-          She can converse in 7 different languages: <strong>Japanese, English, French, German, Hindi, Gujarati,</strong> and <strong>Urdu</strong>.
+          She can converse in seven different languages: <strong>Japanese, English, French, German, Hindi, Gujarati,</strong> and <strong>Urdu</strong>.
         </p>
       </div>
 
